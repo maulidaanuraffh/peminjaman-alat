4345338f1575698ab767411cda96930c501ad0da
Maulida Nur Afifah <maulidaaa.nurafifah@gmail.com>
2024-03-05 20:58:52 +0700

    layout update status peminjaman

---
+++ app/src/main/java/com/maulida/kinanti/peminjamanalat/data/Send.java
@@ -0,0 +1,36 @@
+package com.maulida.kinanti.peminjamanalat.data;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Send extends StringRequest {
+    private static final String Url = "http://192.168.0.105/pinjambarang/update.php";
+    private final String code;
+    private final Response.Listener<String> listener;
+
+    public Send(String code, Response.Listener<String> listener) {
+        super(Request.Method.POST, Url, listener, null);
+        this.code = code;
+        this.listener = listener;
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<>();
+        params.put("id_pinjam", code);
+        return params;
+    }
+
+    @Override
+    protected void deliverResponse(String response) {
+        super.deliverResponse(response);
+        Log.d("UpdateActivity", "Server Response: " + response);
+        listener.onResponse(response);
+    }
+}
--- app/src/main/java/com/maulida/kinanti/peminjamanalat/ui/UpdateActivity.java
+++ app/src/main/java/com/maulida/kinanti/peminjamanalat/ui/UpdateActivity.java
@@ -1,16 +1,83 @@
 package com.maulida.kinanti.peminjamanalat.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.maulida.kinanti.peminjamanalat.R;
+import com.maulida.kinanti.peminjamanalat.data.Send;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class UpdateActivity extends AppCompatActivity {
+    private static final int VIEW = 101;
+    private String code;
+
+    private TextView tvKode, tvNama, tvNomor, tvTanggal, tvBarang, tvStatus, btUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+        Bundle b = getIntent().getExtras();
+        if (b != null) {
+            code = b.getString("id_pinjam");
+        }
+
+        tvKode = findViewById(R.id.tvkode);
+        tvNama = findViewById(R.id.tvnama);
+        tvNomor = findViewById(R.id.tvnomor);
+        tvTanggal = findViewById(R.id.tvtanggal);
+        tvBarang = findViewById(R.id.tvbarang);
+        tvStatus = findViewById(R.id.tvstatus);
+
+        tvKode.setText(code);
+        tvNama.setText(b.getString("nama_pinjam"));
+        tvNomor.setText(b.getString("no_hp"));
+        tvTanggal.setText(b.getString("tgl_pinjam"));
+        tvBarang.setText(b.getString("kode_barang"));
+        tvStatus.setText(b.getString("status"));
+
+        btUpdate = findViewById(R.id.btupdate);
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update(view);
+            }
+        });
     }
+
+    public void update(View view) {
+        Response.Listener<String> lis = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject json = new JSONObject(response);
+                    boolean success = json.getBoolean("success");
+                    if (success) {
+                        Intent intent = new Intent(UpdateActivity.this, DaftarPeminjamanActivity.class);
+                        startActivityForResult(intent, VIEW);
+                        finish();
+                    } else {
+                        Toast.makeText(UpdateActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(UpdateActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        Send send = new Send(code, lis);
+        RequestQueue request = Volley.newRequestQueue(this);
+        request.add(send);
+    }
+
 }
